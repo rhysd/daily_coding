078b4bea748591f70c68e74491ba3a85ae8a8ac1
rhysd <lin90162@yahoo.co.jp>
2012-05-13 04:50:12 +0900

    プロフィール強化

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -8,7 +8,8 @@ class ProfileController < ApplicationController
   def stared_codes
     @uid = params[:user_id]
     @user = User.find_by_twitter_id params[:user_id]
-    stared_answer_ids = Fav.find_by_from(params[:user_id]).map{|t| t.answer_id}
+    stared_answer_ids = Fav.find_by_from(params[:user_id])
+    stared_answer_ids = stared_answer_ids ? stared_answer_ids.map{|t| t.answer_id} : []
     begin
       @stared_answers = Answer.find stared_answer_ids
     rescue ActiveRecord::RecordNotFound
