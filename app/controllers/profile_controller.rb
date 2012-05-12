0e24bcb2e5b2e5249bbdec06be4e07e1081ae680
rhysd <lin90162@yahoo.co.jp>
2012-05-13 02:29:27 +0900

    profile 表示のためのデータ取得用コントローラ

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,16 @@
+class ProfileController < ApplicationController
+  def index
+    @screen_name = params[:user].login
+    @icon_url = params[:user].profile_image_url
+    @profile_description = params[:user].description
+  end
+
+  def codes
+    @my_answers = Answer.find_by_user params[:user].twitter_id
+  end
+
+  def stared_codes
+    stared_answer_ids = Fav.find_by_from(params[:user]).map{|t| t.answer_id}
+    @stared_answers = Answer.find stared_answer_ids
+  end
+end
