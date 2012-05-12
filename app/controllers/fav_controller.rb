b9c58449b422d4f2194ba3cf3ee9be33154244ea
rhysd <lin90162@yahoo.co.jp>
2012-05-12 19:21:46 +0900

    星をクリックした時の動作を定義する fav_controller

--- app/controllers/fav_controller.rb
+++ app/controllers/fav_controller.rb
@@ -1,7 +1,19 @@
 class FavController < ApplicationController
   def create
+      if logged_in?
+          Fav.find_or_create :problem => params[:answer_id], :to => params[:author], :from => current_user.twitter_id
+      else
+          render :status => 403
+      end
   end
 
   def destroy
+      if logged_in?
+          begin
+              Fav.delete :problem => params[:answer_id], :to => params[:author], :from => current_user.twitter_id
+          rescue
+              render :status => 400
+          end
+      end
   end
 end
