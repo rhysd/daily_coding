0343540bc7a1c30c50514d8b243c2b4806cd81fc
rhysd <lin90162@yahoo.co.jp>
2012-05-12 21:00:10 +0900

    fav 対象を to から answer_id に変更．

--- db/migrate/20120512083554_create_favs.rb
+++ db/migrate/20120512083554_create_favs.rb
@@ -2,7 +2,7 @@ class CreateFavs < ActiveRecord::Migration
   def change
     create_table :favs do |t|
       t.integer :problem, :null => false
-      t.integer :to
+      t.integer :answer_id, :null => false
       t.integer :from, :null => false
 
       t.timestamps
