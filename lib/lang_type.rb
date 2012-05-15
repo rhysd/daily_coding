1578adcefd521f45b9d4863fca89352d64c755c1
rhysd <lin90162@yahoo.co.jp>
2012-05-15 20:08:01 +0900

    C++ のコードかどうかを拡張子から判断するようにした．あと Vim の表記が微妙だった（viml）ので，vim の場合も修正

--- lib/lang_type.rb
+++ lib/lang_type.rb
@@ -2,12 +2,22 @@ module DailyCoding
 
     require 'open-uri'
     require 'nokogiri'
-    require 'errors.rb'
+    require './errors.rb'
 
     def lang_type(url)
         doc = Nokogiri::HTML open(url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE)
-        doc.xpath('//div[@class="file"]/div').each do |div|
-            return $1 if div.get_attribute("class") =~ /^data type-(.*)$/
+        result = doc.xpath('//div[@class="file"]/div').each do |div|
+            if div.get_attribute("class") =~ /^data type-(.*)$/
+                type = $1
+                case type
+                when "c"
+                    ext = File.extname doc.xpath('//div[@class="file"]')[0].get_attribute("id")
+                    type = "cpp" if %w[ .cpp .cc .cxx ].include? ext
+                when "viml"
+                    type = "vim"
+                end
+                return type
+            end
         end
         raise LangTypeNotFound,url
     end
