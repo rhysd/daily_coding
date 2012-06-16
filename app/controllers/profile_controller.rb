67906e1d2a09cf302e02fb02fdc504b23f93cfca
y_uuki <yuki.tsubo@gmail.com>
2012-06-16 19:03:34 +0900

    投稿ユーザのプロフィール画像表示. Profileが見れなくなってたので修正

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -14,7 +14,7 @@ class ProfileController < ApplicationController
     @user = User.find_by_twitter_id params[:user_id]
     @my_answers = Answer.find_by_user_id(@user.id) || []
     @my_answers = @my_answers.class == Array ? @my_answers : [@my_answers]
-    stared_answer_ids = Fav.find_by_from(params[:user_id]) || []
+    stared_answer_ids = Fav.find_by_user_id(params[:user_id]) || []
     stared_answer_ids = stared_answer_ids.class == Array ? stared_answer_ids : [stared_answer_ids]
     begin
       @stared_answers = Answer.find stared_answer_ids
