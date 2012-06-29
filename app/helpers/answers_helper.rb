cb8669a5b48a16cde123c2f433401f9ffd0da090
y_uuki <yuki.tsubo@gmail.com>
2012-06-30 04:30:20 +0900

    匿名ログイン -> 投稿後遷移時にエラーでてたので修正

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -3,7 +3,7 @@ module AnswersHelper
   def current_user_faved?(answer)
     favs = answer.favs
     favs.each do |fav|
-      return true if fav.user_id == current_user.id
+      return true if logged_in? && fav.user_id == current_user.id
     end
     false
   end
