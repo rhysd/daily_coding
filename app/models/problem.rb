f9b6a9f48a2178c1bdc29e012a74184ab6678856
y_uuki <yuki.tsubo@gmail.com>
2012-07-01 16:21:46 +0900

    answersにはproposed_atカラムは無いのでcreated_atに戻した

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,7 +2,7 @@
 
 class Problem < ActiveRecord::Base
   attr_accessible :content, :url, :proposed_at
-  has_many :answers, :order => 'proposed_at DESC'
+  has_many :answers, :order => 'created_at DESC'
 
   def self.today
     Problem.where(proposed: false).order("id ASC").first
