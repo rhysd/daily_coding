93780d95c3954b4beb175c01004362a881d94506
y_uuki <yuki.tsubo@gmail.com>
2012-07-08 00:58:44 +0900

    ProfilesControlelrのspec追加

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe ProfilesController do
+  before(:each) do
+    user = FactoryGirl.create(:user_with_answers)
+    current_user= user
+    FactoryGirl.create(:problem_with_answers)
+  end
+
+  describe "GET 'codes'" do
+    it "returns http success" do
+      get 'codes', params = {:user_id => 1}
+      response.should be_success
+    end
+  end
+
+  describe "GET 'stared_codes'" do
+    it "returns http success" do
+      get 'stared_codes', params = {:user_id => 1}
+      response.should be_success
+    end
+  end
 
 end
