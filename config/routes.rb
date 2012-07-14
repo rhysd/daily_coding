7e1c827b66ca90a5c581471c9ea7dbe2e8900e9b
y_uuki <yuki.tsubo@gmail.com>
2012-07-15 00:17:13 +0900

    devise側で定義していたルーティングを再定義してしまっていたので削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ DailyCoding::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
   devise_scope :user do
-    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
     get '/users/sign_in' => 'users/sessions#new', :as => :new_session
     get '/users/sign_out' => 'devise/sessions#destroy', :as => :destroy_session
   end
